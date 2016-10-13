7c0920f02f3c0e71493419aeb285314ce48c2714
Gustavo Jimenez <61401@kjuhsd.k12.ca.us>
2016-10-13 14:07:25 -0700

    more practice

---
+++ oct13/js/script.js
@@ -0,0 +1,24 @@
+var span = document.querySelectorAll('legend span');
+var input = document.querySelectorAll('legend input');
+// console.log(span);
+console.log(input);
+// for(var i = 0; i < span.length; i++){
+//   span[i].addEventListener('click', function(){
+//     // console.log(this);
+//     this.classList.toggle('placeholder');
+//   });
+// };
+
+for (var i = 0; i < input.length; i++) {
+  input[i].addEventListener('click', function(){
+    // this.classList.toggle('placeholder')
+    console.log(this === document.activeElement);
+    if (this === document.activeElement) {
+      this.previousElementSibling.classList.add('placeholder');
+    }else{
+      for (var j = 0; j < span.length; j++) {
+        span[j].classList.remove('placeholder');
+      }
+    }
+  });
+}
